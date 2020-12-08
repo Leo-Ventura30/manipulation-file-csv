c4e3158af540537a3c364dfe7093494ece39bfc0
leo-ventura30 <leonardo.ventura@outlook.com>
2020-12-07 22:38:11 -0300

    create and write into file csv

---
+++ manipulation_csv.py
@@ -0,0 +1,15 @@
+import csv
+
+with open("data_users.csv", "w") as file:
+    content = csv.writer(file)
+    content.writerow(('Nome', 'Sobrenome', 'Turno'))
+    content.writerow(('James', 'Vieira', 'T1'))
+    content.writerow(('Victor', 'Souza', 'T2'))
+    content.writerow(('Tales', 'Sousa', 'T3'))
+
+
+with open("data_users.csv", "r", encoding="utf8", newline="\r\n") as file:
+    read_file = csv.reader(file)
+    dados = list(read_file)
+
+print(dados)
